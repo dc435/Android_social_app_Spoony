004d46a4418d62521b91896f3c70af58f44f96b0
yvonne tao <yuwtao@student.unimelb.edu.au>
2022-10-09 00:30:07 +1100

    add functions, link activities with each other

--- Spoony (prototype2)/app/src/main/java/com/mobcomp/spoony/NameEntry_P2.java
+++ Spoony (prototype2)/app/src/main/java/com/mobcomp/spoony/NameEntry_P2.java
@@ -16,20 +16,11 @@ public class NameEntry_P2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_entry_p2);
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+   
     }
 
     public void nextToPointP1(View view){
-        Intent intent = new Intent(this, NameEntry_P2.class);
-        EditText editText = (EditText) findViewById(R.id.name_input_p1);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        Intent intent = new Intent(this, com.example.mobilecomp.PointTo_P1.class);
         startActivity(intent);
     }
 }
