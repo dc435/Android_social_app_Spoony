f3963d3ec55b5cc81e51f4b5e3f9e3b49c5d2fc2
Cedric0303 <chenjunli7@gmail.com>
2022-10-12 16:27:56 +1100

    fix boot status in firebase

--- Spoony (prototype2)/app/src/main/java/com/mobcomp/spoony/firebaseHandler.java
+++ Spoony (prototype2)/app/src/main/java/com/mobcomp/spoony/firebaseHandler.java
@@ -50,7 +50,7 @@ public class firebaseHandler {
         qcdb = db.collection("questionCount");
         qCountDoc = null;
         multiQDoc = null;
-        firstBoot = false;
+        firstBoot = true;
         getQuestionCount();
     }
 
