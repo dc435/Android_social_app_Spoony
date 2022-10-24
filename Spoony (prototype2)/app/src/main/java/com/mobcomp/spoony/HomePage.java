0a34679be637b2b00497e8fba8b0c22c1ef9972a
Normie <yangjie_0824@outlook.com>
2022-10-24 21:08:11 +1100

    add animation

--- Spoony (prototype2)/app/src/main/java/com/mobcomp/spoony/HomePage.java
+++ Spoony (prototype2)/app/src/main/java/com/mobcomp/spoony/HomePage.java
@@ -6,16 +6,26 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class HomePage extends AppCompatActivity {
 
     ImageButton start_button;
     ImageButton firebase_button;
     GameDetails gd;
+
+    AnimationDrawable catAnimation;
+    ImageView[] clouds;
+    Animation cloudMove1;
+    Animation cloudMove2;
 
     AudioService audioService;
     ServiceConnection connection = new ServiceConnection() {
@@ -45,6 +55,21 @@ public class HomePage extends AppCompatActivity {
         start_button.setOnClickListener(this::jumpToStart);
         firebase_button.setOnClickListener(this::jumpToFirebase);
 
+        ImageView catView = (ImageView) findViewById(R.id.cat_anim);
+        catView.setBackgroundResource(R.drawable.cat_animation);
+        catAnimation = (AnimationDrawable) catView.getBackground();
+
+        cloudMove1 = AnimationUtils.loadAnimation(this, R.anim.anim_cloud_move1);
+        cloudMove2 = AnimationUtils.loadAnimation(this, R.anim.anim_cloud_move2);
+        cloudMove1.setRepeatCount(Animation.INFINITE);
+        cloudMove2.setRepeatCount(Animation.INFINITE);
+        clouds = new ImageView[5];
+        clouds[0] = (ImageView) findViewById(R.id.imageView4);
+        clouds[1] = (ImageView) findViewById(R.id.imageView2);
+        clouds[2] = (ImageView) findViewById(R.id.imageView7);
+        clouds[3] = (ImageView) findViewById(R.id.imageView3);
+        clouds[4] = (ImageView) findViewById(R.id.imageView);
+
         gd = new GameDetails();
     }
 
@@ -53,12 +78,22 @@ public class HomePage extends AppCompatActivity {
         super.onStart();
         Intent intent = new Intent(this, AudioService.class);
         bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        catAnimation.start();
+        clouds[0].startAnimation(cloudMove1);
+        clouds[1].startAnimation(cloudMove2);
+        clouds[2].startAnimation(cloudMove2);
+        clouds[3].startAnimation(cloudMove1);
+        clouds[4].startAnimation(cloudMove2);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         unbindService(connection);
+        catAnimation.stop();
+        for (ImageView v: clouds) {
+            v.clearAnimation();
+        }
     }
 
     public void jumpToStart(View view) {
